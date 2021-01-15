9147fa44f526d4cc10305228fbcc0c510762199d
STEEDCLINT ALEX <steed@unist.ac.kr>
2021-01-15 14:19:24 +0900

    Can serialize classes and structs in the editor.

---
+++ Assets/SerializingObjectsInEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerializingObjectsInEditor : MonoBehaviour
+{
+	public int i;
+	public TestClass testClass;
+	public UserInformation user;
+ 
+}
+[System.Serializable]
+public class TestClass
+{
+	public int i;
+	float f;
+}
+[System.Serializable]
+public struct UserInformation
+{
+	public string username;
+	float xp;
+}
